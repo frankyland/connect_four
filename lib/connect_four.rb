617067d7072cecb203e439086c0ff89d5c15716e
frankyland <arkoftruth@gmail.com>
2012-04-02 14:42:03 +0300

    Update lib/connect_four.rb

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,43 +1,9 @@
+require 'gamefield'
+require 'player'
+require 'logic'
+
 module ConnectFour
   def self.hello_world
     "hello world"
   end
-  class GameField
-
-    # Constants for Field rows and columns
-    Rows =8
-    Cols =8
-
-    #Start Index for the Koordinate of the Field
-    i,j =0
-
-
-
-    #Defined a 2 Dimensional Array
-      def [](rows, cols)
-      end
-
-    #Filled the 8x8 Field with the "." Symbol
-
-    while  i < Rows do
-
-  j=0
-
-	while j < Cols do
-
-		cf.[](i,j) <= '.' 
-
-		j += 1
-
-	end
-
-	i += i + 1
-
-    end
-
-
-
-  end
-  
-  #ToDo class player with player and turns
 end
